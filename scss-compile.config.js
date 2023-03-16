faca9dd0e2ba141be0cb46d5fa1d8b6ceb953fca
Saltssaumure <saltssaumure@gmail.com>
2023-03-16 00:34:03 +0000

    Add scss compiler

---
+++ scss-compile.config.js
@@ -0,0 +1,13 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        /** The official name of your theme. */
+        name: "IEYTD",
+        /** The name of your base scss file. */
+        scss: "main",
+        /** The name of your theme repository. */
+        repo: "ieytd-discord-theme",
+        /** The version of your theme. */
+        version: "1.2.0"
+    }
+};
